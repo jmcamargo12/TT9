b65fa871c792fe1e1ea61e26a08a6a6d84050d17
jonathan manuel camargo labeau <jmcamargo12@gmail.com>
2020-05-10 10:28:35 -0400

    br

--- server.js
+++ server.js
@@ -27,83 +27,83 @@ if (process.env.JAWSDB_URL) {
     password: "root",
     database: "burgersdb",
   });
+}
 
-  connection.connect(function (err) {
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the plans in it.
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM burgers;", function (err, data) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+      return res.status(500).end();
     }
 
-    console.log("connected as id " + connection.threadId);
+    res.render("index", { burgers: data });
   });
+});
 
-  // Use Handlebars to render the main index.html page with the plans in it.
-  app.get("/", function (req, res) {
-    connection.query("SELECT * FROM burgers;", function (err, data) {
+// Create a new plan
+app.post("/api/burgers", function (req, res) {
+  connection.query(
+    "INSERT INTO burgers (burger) VALUES (?)",
+    [req.body.burger],
+    function (err, result) {
       if (err) {
         return res.status(500).end();
       }
 
-      res.render("index", { burgers: data });
-    });
-  });
+      // Send back the ID of the new plan
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    }
+  );
+});
 
-  // Create a new plan
-  app.post("/api/burgers", function (req, res) {
-    connection.query(
-      "INSERT INTO burgers (burger) VALUES (?)",
-      [req.body.burger],
-      function (err, result) {
-        if (err) {
-          return res.status(500).end();
-        }
-
-        // Send back the ID of the new plan
-        res.json({ id: result.insertId });
-        console.log({ id: result.insertId });
+// Update a plan
+app.put("/api/burgers/:id", function (req, res) {
+  connection.query(
+    "UPDATE burgers SET burger = ? WHERE id = ?",
+    [req.body.burger, req.params.id],
+    function (err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      } else if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
       }
-    );
-  });
+      res.status(200).end();
+    }
+  );
+});
 
-  // Update a plan
-  app.put("/api/burgers/:id", function (req, res) {
-    connection.query(
-      "UPDATE burgers SET burger = ? WHERE id = ?",
-      [req.body.burger, req.params.id],
-      function (err, result) {
-        if (err) {
-          // If an error occurred, send a generic server failure
-          return res.status(500).end();
-        } else if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+// Delete a plan
+app.delete("/api/burgers/:id", function (req, res) {
+  connection.query(
+    "DELETE FROM burgers WHERE id = ?",
+    [req.params.id],
+    function (err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      } else if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
       }
-    );
-  });
+      res.status(200).end();
+    }
+  );
+});
 
-  // Delete a plan
-  app.delete("/api/burgers/:id", function (req, res) {
-    connection.query(
-      "DELETE FROM burgers WHERE id = ?",
-      [req.params.id],
-      function (err, result) {
-        if (err) {
-          // If an error occurred, send a generic server failure
-          return res.status(500).end();
-        } else if (result.affectedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
-      }
-    );
-  });
-
-  // Start our server so that it can begin listening to client requests.
-  app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-}
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
