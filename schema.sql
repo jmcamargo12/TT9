8671333fd482d16eb508e3fd464289c9014deea0
jonathan manuel camargo labeau <jmcamargo12@gmail.com>
2020-05-10 10:15:48 -0400

    d

---
+++ schema.sql
@@ -0,0 +1,17 @@
+
+
+
+  CREATE DATABASE burgersdb;
+
+  USE burgersdb;
+
+  -- Create the table plans.
+  CREATE TABLE burgers
+  (
+    id int NOT NULL
+    AUTO_INCREMENT,
+  burger varchar
+    (255) NOT NULL,
+  PRIMARY KEY
+    (id)
+);
